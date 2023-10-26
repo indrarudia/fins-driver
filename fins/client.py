a254d85bcca3fc02edeed074a84193e17d325421
Indra Rudianto <indrarudianto.official@gmail.com>
2023-10-26 13:32:07 +0700

    Add mode option

--- fins/client.py
+++ fins/client.py
@@ -1,4 +1,5 @@
 import socket
+from typing import Literal
 
 from .command import Command
 from .response import Response
@@ -6,13 +7,22 @@ from .response import Response
 
 class Client:
     def __init__(
-        self, host: str = "127.0.0.1", port: int = 9600, timeout: int = 2000
+        self,
+        host: str = "127.0.0.1",
+        port: int = 9600,
+        timeout: int = 2000,
+        mode: Literal["tcp", "udp"] = "udp",
     ) -> None:
         self.host = host
         self.port = port
         self.timeout = timeout
-        self.buffer_size = 4096
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.buffer_size: int = 4096
+        if mode == "udp":
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        elif mode == "tcp":
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            raise ValueError(f"Unknown client mode: {mode}")
 
     def send(self, command: Command) -> Response:
         self._socket.send(command.data)
